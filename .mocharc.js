d5cf2506e9b4c0590f4f36e3d34d1ab0d756f27f
Wolfgang Sassik <wolfgang@sassik.at>
2021-09-30 13:12:43 +0200

    WIP

--- .mocharc.js
+++ .mocharc.js
@@ -2,13 +2,3 @@ const chai = require("chai");
 
 chai.use(require("chai-as-promised"));
 chai.use(require("sinon-chai"));
-
-module.exports = {
-	parallel: true,
-	recursive: true,
-	extension: ["js", "ts"],
-	require: [
-		"ts-node/register",
-		"source-map-support/register"
-	]
-};
--- test/app.test.ts
+++ test/app.test.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import app from "../src/app";
 
 describe("app", async () => {
-	it("is a function", () => {
+	it("is a function", async () => {
 		expect(app).to.be.a("function");
 	});
 });

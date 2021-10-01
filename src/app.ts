2d276132dcb0f81e5706b75b35fa7f497aed5aa9
Sassik Wolfgang (BEG/ENG13-Wi) <Wolfgang.Sassik@at.bosch.com>
2021-10-01 12:12:29 +0200

    async example

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,20 @@ export default (function (this: Application): Application {
 	this.enable("strict routing");
 	this.disable("x-powered-by");
 
-	// this.use(express.json());
+	this.use(express.json());
 
-	this.use((req, res, next) => {
-		// next();
-		// res.end();
+	this.use(async (req, res, next) => {
+		try {
+			// do some work and potentially call...
+			res.end();
+		} catch (error) {
+			// in case the block above throws an error, we NEED to inform expess about it, by calling...
+			return next(error);
+		}
 	});
 
-	// this.use(notFoundHandler());
-	// this.use(errorHandler());
+	this.use(notFoundHandler());
+	this.use(errorHandler());
 
 	return this;
 }.call(express()));
